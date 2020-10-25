daf6f7ae4994a1b5f2444343f108633b42e2d7f5
TristanBeuzelin <70806113+TristanBeuzelin@users.noreply.github.com>
2020-10-25 11:18:32 +0100

    Voila voila

--- Projet_maths.py
+++ Projet_maths.py
@@ -47,32 +47,108 @@ def Newton_steps(F, x0, y0, eps, N,file):
             raise ValueError(f"no convergence in {N} steps.")
 
 #Level curve fonctionne. Problème: la fonction rebondit aux croisements et y fait des aller retours.
-def level_curve(f,f_cont, x, y, N=150, eps=0.01):
+def level_curve(f,f_cont, x, y,oversampling, N=150, eps=0.01):
     tab = np.zeros(shape = (2,N), dtype = float)
+    grad_f = grad(f)
     tab[0,0] = x
     x_start = x
     tab[1,0] = y
     y_start = y
+    g = grad_f(x,y)
+    g_norm = delta*g/np.linalg.norm(g)
+    g_norm_ant = g_norm
     global x0 
     global y0
     for i in range(1,N):
-        grad_f = grad(f)
         g = grad_f(x,y)
         g_norm = delta*g/np.linalg.norm(g)
         J_f = J(f_cont)
         x,y = Newton(f_cont,x+g_norm[1],y-g_norm[0],eps,N)
-        # J'empêche la fonction de faire plusieurs tours de courbe de niveau.
-        if abs(x-x_start) < delta/2 and abs(y-y_start) < delta/2:
-            break_index = i
-            tab = tab[:,:i]
-            break
         tab[0,i] = x
         tab[1,i] = y
+        """
+        if i != 1:
+            if intersection_3([(tab[0,0],tab[1,0]),(tab[0,1],tab[1,1])],[(tab[0,i-1],tab[1,i-1]),(tab[0,i],tab[1,i])]):
+                tab = tab[:,:i]
+                return tab
+        """
+        if oversampling > 1:
+            overtab = np.zeros(shape = (2,oversampling), dtype = float)
+            k = 0
+            for t in np.linspace(0.0,1.0,oversampling):
+                gam = gamma(t,np.array([tab[0,i-1],tab[1,i-1]]),np.array([tab[0,i],tab[1,i]]),
+                np.array([g_norm_ant[1],-g_norm_ant[0]]),np.array([g_norm[1],-g_norm[0]]))
+                overtab[0,k] = gam[0]
+                overtab[1,k] = gam[1]
+                k += 1
+            tab = np.column_stack([tab,overtab])
+        g_norm_ant = g_norm       
         # Déclarer les points obetnus en global permet de les garder facilement pour la boucle suivante,
         # pour calculer la contrainte du cercle sans prise de tête avec les arguments qui posent problème
         # dans le calcul du jacobien.
         x0,y0 = x,y
     return tab
+
+#Implémentation du chemin gamma(t).
+def gamma(t,P1,P2,u1,u2):
+    a = P1[0]
+    d = P1[1]
+    matrice = np.array([u1[0],u2[0],u1[1],u2[1]]).reshape((2,2))
+    if np.linalg.det(matrice) != 0:
+        lambmu = 2*np.dot(np.linalg.inv(matrice),(P2-P1))
+        b = lambmu[0]*u1[0]
+        e = lambmu[0]*u1[1]
+        c = (lambmu[1]*u2[0]-lambmu[0]*u1[0])/2
+        f = (lambmu[1]*u2[1]-lambmu[0]*u1[1])/2
+        x = a+b*t+c*t**2
+        y = d+e*t+f*t**2
+    else:
+        x = P1[0]+t*(P2[0]-P1[0])
+        y = P1[1]+t*(P2[1]-P1[1])
+    return np.array([x,y]).reshape((2,1))
+
+# Intersection testée, ne fonctionne pas sur les segments qui se superposent.
+def intersection_2(a1,a2,b1,b2):
+    if a2[0]-a1[0] != 0:
+        coef1 = (a2[1]-a1[1])/(a2[0]-a1[0])
+        ord1 = a1[1]-a1[0]*coef1
+    if b2[0]-b1[0] != 0:
+        coef2 = (b2[1]-b1[1])/(b2[0]-b1[0])
+        ord2 = b1[1]-b1[0]*coef1
+    if coef1 == coef2 and ord1 != ord2:
+        return False
+    if coef1 == coef2 and ord2 == ord2:
+        if (min(b1[0],b2[0]) < a1[0] < max(b1[0],b2[0]) or min(b1[0],b2[0]) < a2[0] < max(b1[0],b2[0]) 
+        or min(a1[0],a2[0]) < b1[0] < max(a1[0],a2[0]) or min(a1[0],a2[0]) < b2[0] < max(a1[0],a2[0])):
+            return True
+        else:
+            return False
+    I1 = (ord2 - ord1) / (coef1 - coef2)
+    if (I1 < max( min(a1[0],a2[0]), min(b1[0],b2[0]))) or (I1 > min( max(a1[0],a2[0]), max(b1[0],b2[0]))):
+        return False
+    else:
+        return True
+
+def intersection(a1,a2,b1,b2):
+    K = ((a2[1]-a1[1])/(a2[0]-a1[0])-(b2[1]-b1[1])/(b2[0]-b1[0]))
+    if K != 0:
+        xi = (((a2[1]-a1[1])/(a2[0]-a1[0]))*a1[0]+a1[1]-((b2[1]-b1[1])/(b2[0]-b1[0]))*b1[0]-b1[1]) / K
+    if (xi < max( min(a1[0],a2[0]), min(b1[0],b2[0]))) or (xi > min( max(a1[0],a2[0]), max(b1[0],b2[0]))):
+        return False
+    else:
+        return True
+
+def intersection_3(s0,s1):
+    dx0 = s0[1][0]-s0[0][0]
+    dx1 = s1[1][0]-s1[0][0]
+    dy0 = s0[1][1]-s0[0][1]
+    dy1 = s1[1][1]-s1[0][1]
+    p0 = dy1*(s1[1][0]-s0[0][0]) - dx1*(s1[1][1]-s0[0][1])
+    p1 = dy1*(s1[1][0]-s0[1][0]) - dx1*(s1[1][1]-s0[1][1])
+    p2 = dy0*(s0[1][0]-s1[0][0]) - dx0*(s0[1][1]-s1[0][1])
+    p3 = dy0*(s0[1][0]-s1[1][0]) - dx0*(s0[1][1]-s1[1][1])
+    return (p0*p1<=0) & (p2*p3<=0)
+
 
 # J'implémente la contrainte pour que le point reste sur le cercle de centre le point précédent de rayon delta. La méthode de Newton
 # cherchera donc un point exclusivement sur ce cercle. Garantit la distance entre les points de la courbe de niveau.
@@ -132,14 +208,14 @@ def display_contour(f, x, y, levels,array,title):
 # Déclaration du point de départ de la recherche de courbe de niveau et des différents paramètres utiles au programme.
 # J'ai passés la valeur c de la ligne de niveau et l'écart delta en global pour plus de clarté dans mon code et éviter les arguments
 # récurrents et inutiles.
-x = 2.0
-y = 2.0
+x = 1.0
+y = 1.0
 eps = 0.01
-N = 100
+N = 1000
 global c
 global delta
-c = -2.4
-delta = 0.1
+c = 1.5
+delta = 0.05
 
 """
 #Analyse de l'écart entre la solution exacte et l'approximation en fonction du pt de départ
@@ -157,9 +233,30 @@ with open("Newton_test.txt","w") as data:
 list = [0.8,-1.0]
 for i in list:
     Newton_steps(f,i,i,eps,N,"Newton_steps.txt")
-display_contour(f1,x=np.linspace(-1.0, 1.0, 100),y=np.linspace(-1.0, 1.0, 100),levels=10,file ="Newton_steps.txt",title="Méthode de Newton pour les points (0.8,0.8) et (-1.0,-1.0)"+"\n"+"Fonction quadratique")
+display_contour(f1,x=np.linspace(-1.0, 1.0, 100),y=np.linspace(-1.0, 1.0, 100),
+levels=10,file ="Newton_steps.txt",
+title="Méthode de Newton pour les points (0.8,0.8) et (-1.0,-1.0)"+"\n"+"Fonction quadratique")
 """
+
 #Détermination des courbes de niveau
-x0,y0 = Newton(f3_start,x,y,eps,N)
-tableau = level_curve(f3,f3_cont,x0,y0)
-display_contour(f3,x=np.linspace(-5.0, 5.0, 100),y=np.linspace(-5.0, 5.0, 100),levels=5 ,array=tableau,title="Méthode de Newton pour les points (0.8,0.8) et (-1.0,-1.0)"+"\n"+"Fonction quadratique")
+x0,y0 = Newton(f1_start,x,y,eps,N)
+tableau = level_curve(f1,f1_cont,x0,y0,10)
+#print(tableau)
+display_contour(f1,x=np.linspace(-1.0, 1.0, 100),y=np.linspace(-1.0, 1.0, 100),
+levels=5 ,array=tableau,
+title="Méthode de Newton pour les points (0.8,0.8) et (-1.0,-1.0)"+"\n"+"Fonction quadratique")
+
+"""
+# Représentation du fonctionnement de gamma
+P1 = np.array([11.0,11.0])
+P2 = np.array([15.0,24.0])
+u1 = np.array([0.7,4.3])
+u2 = np.array([1.0,0.6])
+ax = plt.axes()
+ax.arrow(P1[0],P1[1],u1[0],u1[1],width = 0.05)
+ax.arrow(P2[0],P2[1],u2[0],u2[1],width = 0.05)
+for t in np.linspace(0.0,1.0,100):
+    gam = gamma(t,P1,P2,u1,u2)
+    plt.scatter(gam[0],gam[1])
+plt.show()
+"""
